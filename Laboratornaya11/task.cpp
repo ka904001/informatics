4cddd6fe3ba0255edbf6d30a2c41dfd1608e45f2
ka904001 <ka904001@gmail.com>
2018-12-17 20:25:03 +0300

    yesssss

--- Laboratornaya11/task.cpp
+++ Laboratornaya11/task.cpp
@@ -1,22 +1,34 @@
 #include <ctime>
 #include <iostream>
 #include <vector>
-
+#include <limits>
 using namespace std;
 
 int main() {
-  char n = '7';
-  cin >> n;
-  while (!((int)n >= 50 && (int)n <= 53)) {
+  //char n = '7';
+  string str;
+  cout<<"Enter value in range [2, 5]"<<endl;
+  //cin.ignore(2, istream::eofbit);
+  cin>>str;
+  //cin.clear();
+  cin.ignore(numeric_limits<std::streamsize>::max(), '\n');
+  //cin.ignore(1, istream::eofbit);
+  while (!((int)str[0] >= 50 && (int)str[0] <= 53&&str.length()==1)) {
     cout << "Invalid input, try again" << endl;
-    cin >> n;
+    //cin.ignore(2, istream::eofbit);
+    cin>>str;
+    //cin.get(n);
+    cin.ignore(numeric_limits<std::streamsize>::max(), '\n');
+    //cin.clear();
+    //cin.ignore(256);
+    //cin.ignore(1, istream::eofbit);
   }
   string a[5][5];
   string temp = "0000";
   srand(time(0));
   // cout<<"check"<<endl;
-  for (int i = 0; i < (int)n - 48; i++) {
-    for (int k = 0; k < (int)n - 48; k++) {
+  for (int i = 0; i < (int)str[0] - 48; i++) {
+    for (int k = 0; k < (int)str[0] - 48; k++) {
       for (int j = 0; j < 4; j++) {
         // cout<<"check"<<endl;
         temp[j] = (char)(65 + rand() % 26);
@@ -28,18 +40,18 @@ int main() {
     cout << endl;
   }
   int mas[25]{0};
-  for (int i = 0; i < (int)n - 48; i++) {
-    for (int k = 0; k < (int)n - 48; k++) {
+  for (int i = 0; i < (int)str[0] - 48; i++) {
+    for (int k = 0; k < (int)str[0] - 48; k++) {
       int result = 0;
       for (int j = 0; j < 4; j++) {
         if (a[i][j][k] == 'A' || a[i][j][k] == 'E' || a[i][j][k] == 'I' ||
             a[i][j][k] == 'O' || a[i][j][k] == 'U')
           result++;
       }
-      mas[i * ((int)n - 48) + k] = result;
+      mas[i * ((int)str[0] - 48) + k] = result;
     }
   }
-  for (int i = 0; i < ((int)n - 48) * ((int)n - 48); i++) {
+  for (int i = 0; i < ((int)str[0] - 48) * ((int)str[0] - 48); i++) {
     cout << mas[i] << ' ';
   }
   return 0;
